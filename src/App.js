59206b263d4995a32f482f748a07aa9707b8571a
hoang0508 <nguyenhuyhoang05082001@gmail.com>
2021-09-05 22:38:23 +0700

    hh2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> Hello world with Reactjs</p>{" "}
+        <p> Hello world with Reactjs huy hoang</p>{" "}
         <a
           className="App-link"
           href="https://reactjs.org"
